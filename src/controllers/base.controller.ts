1af30418581468dd3e7b172965380edc338ce586
bakhshabadi <bakhshabadi.javad@gmail.com>
2022-12-12 07:22:00 +0330

    fix

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,9 +1,6 @@
-import { Logger } from "winston";
 declare var  Logger:any;
 
 export class BaseController {
-    protected logger: Logger
     constructor(){
-        this.logger=new Logger();
     }
 }
