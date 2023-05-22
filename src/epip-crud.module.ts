4e6f4090722c27967989e7e7707c6a3bd91c8de8
bakhshabadi <bakhshabadi.javad@gmail.com>
2023-05-22 20:22:41 +0430

    fix

--- src/epip-crud.module.ts
+++ src/epip-crud.module.ts
@@ -10,12 +10,12 @@ import to from 'await-to-js';
 export class EpipCrudModule {
   static register(): DynamicModule {
     SelectQueryBuilder.prototype.getMapRawManyAndCount = async function (type: any): Promise<[any[], number]> {
-      let [err, count] = await to(this.select((type as any).prototype._params).getCount())
+      let [err, count] = await to(this.select((type as any).prototype._params).groupBy((type as any).prototype._params.map(f=>f.split(" as ")[0]).join()).getCount())
       if (err) {
         throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR)
       }
 
-      let [err1, data] = await to(this.select((type as any).prototype._params).getRawMany())
+      let [err1, data] = await to(this.select((type as any).prototype._params).groupBy((type as any).prototype._params.map(f=>f.split(" as ")[0]).join()).getRawMany())
       if (err1) {
         throw new HttpException(err1, HttpStatus.INTERNAL_SERVER_ERROR)
       }
@@ -24,7 +24,7 @@ export class EpipCrudModule {
     };
 
     SelectQueryBuilder.prototype.getMapRawMany = async function  (type: any): Promise<any[]> {
-      return this.select((type as any).prototype._params).getRawMany();
+      return this.select((type as any).prototype._params).groupBy((type as any).prototype._params.map(f=>f.split(" as ")[0]).join()).getRawMany();
     };
     return {
       module: EpipCrudModule,
