5928a891b071d9ead23a7abbed22ec3041d8c2ac
bakhshabadi <bakhshabadi.javad@gmail.com>
2024-01-09 14:07:13 +0330

    fix

--- src/decorators/swg-crud.decorator.ts
+++ src/decorators/swg-crud.decorator.ts
@@ -1,16 +1,31 @@
-import { applyDecorators, Delete, Get, Inject, Patch, Post, Put, Type, UseFilters } from "@nestjs/common";
-import { ApiOkResponse, ApiParam, getSchemaPath } from "@nestjs/swagger";
-import { IResponse, IResponseAll } from "../types/res.interface";
+import {
+  applyDecorators,
+  Delete,
+  Get,
+  Inject,
+  Patch,
+  Post,
+  Put,
+  Type,
+  UseFilters,
+} from '@nestjs/common';
+import { ApiOkResponse, ApiParam, getSchemaPath } from '@nestjs/swagger';
+import { IResponse, IResponseAll } from '../types/res.interface';
 
-const getParams = url => {
-  let arr = url.split('/').filter(f => f.substring(0, 1) == ":").map(f => f.substring(1));
-  return arr.map(f => ApiParam({
-    name: f,
-    type: 'string'
-  }))
-}
+const getParams = (url) => {
+  let arr = url
+    .split('/')
+    .filter((f) => f.substring(0, 1) == ':')
+    .map((f) => f.substring(1));
+  return arr.map((f) =>
+    ApiParam({
+      name: f,
+      type: 'string',
+    }),
+  );
+};
 
-export class AnyResponse { }
+export class AnyResponse {}
 
 const getResponse = (model: any) => {
   if (model) {
@@ -19,26 +34,28 @@ const getResponse = (model: any) => {
       {
         properties: {
           result: {
-            $ref: getSchemaPath(model.name !== 'Object' ? model.name : AnyResponse.name),
+            $ref: getSchemaPath(
+              model.name !== 'Object' ? model.name : AnyResponse.name,
+            ),
           },
         },
-      }
-    ]
+      },
+    ];
   } else {
-    return [{ $ref: getSchemaPath(IResponse) }]
+    return [{ $ref: getSchemaPath(IResponse) }];
   }
-}
+};
 
 export const ApiGetAll = <TModel extends Type<any>>(
   model: TModel,
-  path?: string
+  path?: string,
 ) => {
   if (!path) {
-    path = "/";
+    path = '/';
   }
   let s = getSchemaPath(IResponseAll);
   return applyDecorators(
-    Get(path || "/"),
+    Get(path || '/'),
     ...getParams(path),
     ApiOkResponse({
       schema: {
@@ -54,36 +71,53 @@ export const ApiGetAll = <TModel extends Type<any>>(
           },
         ],
       },
-    })
+    }),
   );
 };
 
 export const ApiGet = <TModel extends Type<any>>(
   model: TModel,
-  path?: string
+  path?: string,
 ) => {
   if (!path) {
-    path = "/:id";
+    path = '/:id';
   }
   return applyDecorators(
     Get(path),
     ...getParams(path),
     ApiOkResponse({
       schema: {
-        allOf: [
-          ...getResponse(model),
-        ],
+        allOf: [...getResponse(model)],
       },
-    })
+    }),
   );
 };
 
 export const ApiPost = <TModel extends Type<any>>(
   model: TModel,
-  path?: string
+  path?: string,
 ) => {
   if (!path) {
-    path = "/";
+    path = '/';
+  }
+
+  return applyDecorators(
+    Post(path),
+    ...getParams(path),
+    ApiOkResponse({
+      schema: {
+        allOf: [...getResponse(model)],
+      },
+    }),
+  );
+};
+
+export const ApiPostAll = <TModel extends Type<any>>(
+  model: TModel,
+  path?: string,
+) => {
+  if (!path) {
+    path = '/';
   }
 
   return applyDecorators(
@@ -92,70 +126,129 @@ export const ApiPost = <TModel extends Type<any>>(
     ApiOkResponse({
       schema: {
         allOf: [
-          ...getResponse(model),
+          { $ref: getSchemaPath(IResponseAll) },
+          {
+            properties: {
+              results: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model.name) },
+              },
+            },
+          },
         ],
       },
-    })
+    }),
   );
 };
 
 export const ApiPut = <TModel extends Type<any>>(
   model: TModel,
-  path?: string
+  path?: string,
 ) => {
   if (!path) {
-    path = "/:id";
+    path = '/:id';
   }
 
   return applyDecorators(
-    Put(path || "/:id"),
+    Put(path || '/:id'),
+    ...getParams(path),
+    ApiOkResponse({
+      schema: {
+        allOf: [...getResponse(model), ,],
+      },
+    }),
+  );
+};
+
+export const ApiPutAll = <TModel extends Type<any>>(
+  model: TModel,
+  path?: string,
+) => {
+  if (!path) {
+    path = '/:id';
+  }
+
+  return applyDecorators(
+    Put(path || '/:id'),
     ...getParams(path),
     ApiOkResponse({
       schema: {
         allOf: [
-          ...getResponse(model), ,
+          { $ref: getSchemaPath(IResponseAll) },
+          {
+            properties: {
+              results: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model.name) },
+              },
+            },
+          },
         ],
       },
-    })
+    }),
   );
 };
 
 export const ApiPatch = <TModel extends Type<any>>(
   model: TModel,
-  path?: string
+  path?: string,
 ) => {
   if (!path) {
-    path = "/:id";
+    path = '/:id';
   }
   return applyDecorators(
-    Patch(path || "/:id"),
+    Patch(path || '/:id'),
+    ...getParams(path),
+    ApiOkResponse({
+      schema: {
+        allOf: [...getResponse(model)],
+      },
+    }),
+  );
+};
+
+export const ApiPatchAll = <TModel extends Type<any>>(
+  model: TModel,
+  path?: string,
+) => {
+  if (!path) {
+    path = '/:id';
+  }
+  return applyDecorators(
+    Patch(path || '/:id'),
     ...getParams(path),
     ApiOkResponse({
       schema: {
         allOf: [
-          ...getResponse(model),
+          { $ref: getSchemaPath(IResponseAll) },
+          {
+            properties: {
+              results: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model.name) },
+              },
+            },
+          },
         ],
       },
-    })
+    }),
   );
 };
 
 export const ApiDelete = <TModel extends Type<any>>(
   model: TModel,
-  path?: string
+  path?: string,
 ) => {
   if (!path) {
-    path = "/:id";
+    path = '/:id';
   }
   return applyDecorators(
-    Delete(path || "/:id"),
+    Delete(path || '/:id'),
     ...getParams(path),
     ApiOkResponse({
       schema: {
-        allOf: [
-          ...getResponse(model),
-        ],
+        allOf: [...getResponse(model)],
       },
-    })
+    }),
   );
 };
